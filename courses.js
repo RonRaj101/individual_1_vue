e8fbddd7ae93e9cf0343c8041ca1fa90dc142c2c
Ronit Rai <43289911+RonRaj101@users.noreply.github.com>
2022-11-15 16:04:35 +0000

    done

---
+++ courses.js
@@ -0,0 +1,105 @@
+var lessons = [
+    {
+        "subject": "Mathematics",
+        "description": "Basics of Arithmetic & Algebra",
+        "location": "London",
+        "price": 49,
+        "total_stock": 5,
+        "stock": 5,
+        "image_url": "images/undraw_mathematics_-4-otb.svg"
+    },
+
+    {
+        "subject": "English",
+        "description": "Basics of Grammar & Pronounciation",
+        "location": "Newcastle",
+        "price": 29,
+        "total_stock": 5,
+        "stock": 5,
+        "image_url": "images/undraw_things_to_say_re_jpcg.svg"
+    },
+    
+    {
+        "subject": "Economics",
+        "description": "Get Started with Macro and Micro Economics",
+        "location": "Birmingham",
+        "price": 99,
+        "total_stock": 5,
+        "stock": 5,
+        "image_url": "images/undraw_discount_d-4-bd.svg"
+    },
+
+    {
+        "subject": "UI Design Fundamentals",
+        "description": "Foundations of Designing UI/UX Designs",
+        "location": "Oxford",
+        "price": 299,
+        "total_stock": 5,
+        "stock": 5,
+        "image_url": "images/undraw_design_process_re_0dhf.svg"
+    },
+
+    {
+        "subject": "Robotics",
+        "description": "Basics of building Hardware and Software Intelligence",
+        "location": "Portsmouth",
+        "price": 149,
+        "total_stock": 5,
+        "stock": 5,
+        "image_url": "images/undraw_artificial_intelligence_re_enpp.svg"
+    },
+
+    {
+        "subject": "Art of Wellbeing",
+        "description": "Explore the tips and techniques of being mentally healthy",
+        "location": "London",
+        "price": 79,
+        "total_stock": 5,
+        "stock": 5,
+        "image_url": "images/undraw_woman_re_afr8.svg"
+    },
+
+    {
+        "subject": "Sketching & Drawing",
+        "description": "Basics of sketching and illustrations",
+        "location": "Bath",
+        "price": 99,
+        "total_stock": 5,
+        "stock": 5,
+        "image_url": "images/undraw_art_lover_re_fn8g.svg"
+    },
+
+    {
+        "subject": "Archaelogy 101",
+        "description": "Opportunity to explore past humanity, from its prehistoric origins to industrialisation and globalisation",
+        "location": "Edinbrough",
+        "price": 199,
+        "total_stock": 5,
+        "stock": 5,
+        "image_url": "images/undraw_treasure_of-9-i.svg"
+    },
+
+    {
+        "subject": "Blockchain & Crypto",
+        "description": "Basics to Advanced of Blockchain Technology",
+        "location": "Brighton",
+        "price": 99,
+        "total_stock": 5,
+        "stock": 5,
+        "image_url": "images/undraw_nakamoto_-2-iv6.svg"
+    },
+
+    {
+        "subject": "Fashion Photography",
+        "description": "Camera Angles, Lighting and Poses; Start to End",
+        "location": "Bristol",
+        "price": 59,
+        "total_stock": 5,
+        "stock": 5,
+        "image_url": "images/undraw_fashion_photoshoot_mtq8.svg"
+    }
+
+
+    
+    
+];
